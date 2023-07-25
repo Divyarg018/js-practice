df053bd1986bf08725f7b41193d38738220f161a
Divya R <kwrdivyaraj100@gmail.com>
2023-07-25 13:11:30 +0530

    modified js file

--- 17/index.js
+++ 17/index.js
@@ -1,18 +1,27 @@
-console.log("This is tutorial 17 on events");
+// console.log("This is tutorial 17 on events");
 
-document.getElementById("heading").addEventListener("click", function(e) {
-  let variable;
+// document.getElementById("heading").addEventListener("click", function(e) {
+//   let variable;
+//   console.log("You have clicked the heading");
+//   // variable = e.target;
+//   // variable = e.target.className;
+//   // variable = Array.from(e.target.classList);
+
+//   // variable = e.target.id;
+//   // variable = e.offsetX;
+//   // variable = e.offsetY;
+//   // variable = e.clientX;
+//   // variable = e.clientY;
+//   // console.log(variable);
+//   // location.href = '//brainsmartlabs.com'
+// });
+
+let evHand = function (e) {
   console.log("You have clicked the heading");
-  variable = e.target;
-  variable = e.target.className;
-  variable = Array.from(e.target.classList);
+  let xAxis = e.offsetX;
+  let yAxis = e.offsetY;
+  console.log("You have clicked your mouse on " + xAxis + " and " + yAxis);
+}
 
-  variable = e.target.id;
-  variable = e.offsetX;
-  variable = e.offsetY;
-  variable = e.clientX;
-  variable = e.clientY;
-  console.log(variable);
-  // location.href = '//brainsmartlabs.com'
-});
-
+let ele = document.getElementById("heading")
+ele.addEventListener("click", evHand);
