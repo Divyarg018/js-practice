6a4754b9d19f0ec0113b2a5e6f5439d2f1e81b7c
Divya R <kwrdivyaraj100@gmail.com>
2023-08-08 12:32:47 +0530

    Local and Session storage in Js

---
+++ 20/index.js
@@ -0,0 +1,21 @@
+let impArray = ['adrak', 'pyaz', 'bhindi'];
+
+
+localStorage.setItem('Name', 'Harry');
+localStorage.setItem('Name2', 'Rohan');
+localStorage.setItem('Sabzi', JSON.stringify(impArray));
+
+
+// localStorage.clear();
+
+
+// localStorage.removeItem('Name2');
+
+
+let name = localStorage.getItem('Name');
+name = JSON.parse(localStorage.getItem('Sabzi'));
+console.log(name);
+
+sessionStorage.setItem('sessionName', 'sHarry');
+sessionStorage.setItem('sessionName2', 'sRohan');
+sessionStorage.setItem('sessionSabzi', JSON.stringify(impArray));
