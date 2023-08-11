f265b6d7bf7dca7ec158bddfb15704e84fc60699
Divya R <kwrdivyaraj100@gmail.com>
2023-08-11 10:17:16 +0530

    Creating an Editable Div Exercise 2: Solution

--- 24/index.js
+++ 24/index.js
@@ -8,15 +8,30 @@ let a;
 a = otherDate.getDay();
 a = otherDate.getDate();
 a = otherDate.getMinutes();
-a = otherDate.getSeconds();
-a = otherDate.getHours();
-a = otherDate.getTime();
-a = otherDate.getMonth();
+//a = otherDate.getSeconds();
+//a = otherDate.getHours();
+//a = otherDate.getTime();
 //console.log(a);
-otherDate.setDate(23);
+//otherDate.setDate(23);
 otherDate.setMonth(0);
-otherDate.setFullYear(1900);
-otherDate.setMinutes(2);
-otherDate.setHours(1);
-otherDate.setSeconds(3);
-console.log(otherDate);
+// otherDate.setFullYear(1900);
+// otherDate.setMinutes(2);
+// otherDate.setHours(1);
+// otherDate.setSeconds(3);
+ //console.log(otherDate);
+
+//  let ms = Date.now();
+//  console.log(ms);
+
+// let demo = Date.parse('Aug 10 2023 00:00:00');
+// console.log(demo);
+
+const b = new Date('10/08/2023 DD/MM/YYYY 00:00:00');
+//let date = b.toLocaleDateString();
+//let date = b.toLocaleString();
+let date = b.toString();
+//let date = b.valueOf();
+console.log(date);
+
+// const date1 = new Date('Aug-11-2022 10:00:00');
+// console.log(date1.valueOf());
