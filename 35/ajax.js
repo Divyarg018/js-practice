cb31c85883088f82548b9662c027a3b289c33af1
Divya R <kwrdivyaraj100@gmail.com>
2023-09-11 13:13:04 +0530

    instantiate an xhr object

--- 35/ajax.js
+++ 35/ajax.js
@@ -1,1 +1,29 @@
 console.log("Ajax");
+
+let fetchBtn = document.getElementById('fetchBtn');
+fetchBtn.addEventListener('click', buttonClickHandler)
+
+
+function buttonClickHandler(){
+    console.log('You have clicked the buttonClick');
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'harry.txt', true);
+
+    xhr.onprogress = function() {
+        console.log('on-progress');
+    }
+    xhr.onreadystatechange = function(){
+        console.log('ready state is', xhr.readyState);
+    }
+    xhr.onload = function(){
+        if(this.status === 200){
+        console.log(this.responseText);
+    }
+    else{
+        console.error('some error occured');
+    }
+}
+
+    xhr.send();
+}
