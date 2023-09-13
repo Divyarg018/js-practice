9be6c11b55f99f4d8b41cc0998c60693e71d2a71
Divya R <kwrdivyaraj100@gmail.com>
2023-09-13 11:57:40 +0530

    arrow functions in js

---
+++ 41/index.js
@@ -0,0 +1,12 @@
+// const harry = () => {
+//     console.log("This is the best person ever");
+// }
+
+//const greet = ()=> "Good Morning";
+const greet = (name, ending)=> " Hi "  +  name  +  ending;
+
+
+
+console.log (greet('harry', ' bye '));
+
+// harry();
