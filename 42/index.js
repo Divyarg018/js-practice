b59c63c29f64ab8ec5d5fe722da4fa33e1c0bf8b
Divya R <kwrdivyaraj100@gmail.com>
2023-09-13 12:47:57 +0530

    fetch api in js

--- 42/index.js
+++ 42/index.js
@@ -3,16 +3,34 @@ let conten = document.getElementById("content");
 
 function getData() {
     console.log("Started getData");
-    url = "https://api.github.com/users";
+    url = "https://api.github.com/users"
     fetch(url).then((response) => {
         console.log("Inside first then");
         return response.json();
-    }).then((data)=>{
+    }).then((data) => {
         console.log("Inside second then");
         console.log(data);
     })
 }
-console.log("Before running getData");
 
-getData();
-console.log("Before after getData");
+function postData() {
+
+    url = "https://dummy.restapiexample.com/api/v1/create";
+    data = '{"name": "fvgbyzx582186", "salary": "123", "age":"23"}'
+    params = {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: data
+    }
+    fetch(url, params).then(response => response.json())
+    .then(data =>console.log(data)
+    )
+}
+// console.log("Before running getData");
+
+// getData();
+// console.log("Before after getData");
+
+postData()
