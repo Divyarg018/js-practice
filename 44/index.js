3ae5779aac31cd4a853081430664e4e2a58b0db1
Divya R <kwrdivyaraj100@gmail.com>
2023-09-14 11:24:36 +0530

    Error Handling and try catch in JS

--- 44/index.js
+++ 44/index.js
@@ -8,11 +8,14 @@ else{
 }
 
 try{
+    null.console;
     console.log("We are inside try block");
-    // functionHarry();
+     functionHarry();
+    
 }catch (error){ 
+    console.log(error);
     console.log('Are you OK?');
-    // console.log(error.name);
+    console.log(error.name);
     console.log(error.message);
 }finally{
     console.log("Finally we will run this");
