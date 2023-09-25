fbfdfe52505fccef33fe2b05e8c81c1eea223e37
Divya R <kwrdivyaraj100@gmail.com>
2023-09-25 10:46:11 +0530

    for in loop

--- 56/index.js
+++ 56/index.js
@@ -1,4 +1,4 @@
-people = ["Harry", "Rohan", "SkillF", "Shumbam", "Vikrant"];
+let people = ["Harry", "Rohan", "SkillF", "Shumbham", "Vikrant"];
 
 
 // for (let index = 0; index < people.length; index++) {
@@ -6,3 +6,29 @@ people = ["Harry", "Rohan", "SkillF", "Shumbam", "Vikrant"];
 //     console.log(element);
     
 // }
+
+let obj = {
+    name: "Harry",
+    language: "JavaScript",
+    hobbies: "Android app development"
+}
+// console.log(obj);
+
+// Iterating an object
+// for (let index = 0; index < Object.keys(obj).length; index++) {
+//     const element = obj[Object.keys(obj)[index]];
+//     console.log(element);
+    
+// }
+
+for(let key in obj){
+    console.log(obj[key]);
+}
+
+myString  = "this is my string";
+
+for(let char in myString){
+    console.log(myString[char]);
+}
+
+// for off
