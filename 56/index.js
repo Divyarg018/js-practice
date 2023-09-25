085c11aa0a919cc5c26a1a727ea8e287b244b86a
Divya R <kwrdivyaraj100@gmail.com>
2023-09-25 10:16:57 +0530

    created index.html file

---
+++ 56/index.js
@@ -0,0 +1,8 @@
+people = ["Harry", "Rohan", "SkillF", "Shumbam", "Vikrant"];
+
+
+// for (let index = 0; index < people.length; index++) {
+//     const element = people[index];
+//     console.log(element);
+    
+// }
