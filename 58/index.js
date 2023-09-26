26b1db91d68c5ee289c869bf5115bba3a91d80b0
Divya R <kwrdivyaraj100@gmail.com>
2023-09-26 11:32:28 +0530

    Js sets

--- 58/index.js
+++ 58/index.js
@@ -12,14 +12,25 @@ mySet.add(false);
 mySet.add('that2');
 console.log('The set looks like this now:', mySet);
 
-// let mySet2 = new Set([1, 45, 'this', false, {a:4, b:8}, 'this']);
-// console.log('New set:', mySet2);
+//use a constructor to initialize the set
+let mySet2 = new Set([1, 45, 'this', false, {a:4, b:8}, 'this']);
+console.log('New set:', mySet2);
 
 
-console.log(mySet.size);
+console.log(mySet.size); // get the size of the set
 
-console.log(mySet.has(34));
+console.log(mySet.has(34));// check whether set has 34 or not
 
 console.log('Before removal', mySet.has('that2'));
-mySet.delete('that2');
+mySet.delete('that2');//remove an element from the set
 console.log('after removal', mySet.has('that2'));
+
+//iterating a set
+// for(let item of mySet){
+//     console.log('Item is :', item);
+// }
+
+mySet.forEach((item)=>{
+    console.log('Item is :', item);
+    
+})
