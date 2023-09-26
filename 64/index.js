fb381c2f9d585f4a6945310f03602e3ed78193c4
Divya R <kwrdivyaraj100@gmail.com>
2023-09-26 15:06:34 +0530

    drag and drop elements with js

---
+++ 64/index.js
@@ -0,0 +1,36 @@
+const imgBox = document.querySelector('.imgBox');
+const whiteBoxes = document.getElementsByClassName('whiteBox');
+
+imgBox.addEventListener('dragstart', (e)=>{
+console.log('Dragstart has been triggered');
+e.target.className += ' hold';
+setTimeout(() => {
+    e.target.className = 'hide';
+}, 0);
+});
+
+imgBox.addEventListener('dragend', (e)=>{
+    console.log('Dragend has been triggered');
+    e.target.className = 'imgBox';
+    
+});
+
+for(whiteBox of whiteBoxes){
+    whiteBox.addEventListener('dragover', (e)=>{
+        e.preventDefault();
+        console.log('Dragover has been triggered');
+
+    });
+    whiteBox.addEventListener('dragenter', (e)=>{
+        console.log('Dragenter has been triggered');
+        e.target.className += ' dashed';
+    });
+    whiteBox.addEventListener('dragleave', (e)=>{
+        console.log('Dragleave has been triggered');
+        e.target.className = 'whiteBox'
+    });
+    whiteBox.addEventListener('drop', (e)=>{
+        console.log('Drop has been triggered');
+        e.target.append(imgBox)
+    })
+}
